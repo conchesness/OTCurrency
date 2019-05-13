a890651941cc3b12235a493a94e7d4ae7ae1159e
Stephen Wright <stephen.wright@ousdnet>
2019-05-12 18:46:05 -0700

    Fixing stuff that Jackton broke

--- app/routes/index.py
+++ app/routes/index.py
@@ -44,7 +44,6 @@ def index():
 
 @app.route('/transvote/<transID>/<vote>')
 def transvote(transID, vote):
-    currUser = User.objects.get(pk=transID)
     transaction = Transaction.objects.get(pk=transID)
     currUser = User.objects.get(googleid=session['googleID'])
 
